e9ecf82caf67f35388955699b11973c33559feda
Dan Mønster <dan@m0nster.dk>
2017-10-10 15:11:42 +0200

    Script for sorting feedback

---
+++ sort_feedback.sh
@@ -0,0 +1,12 @@
+# Fid and print the positive feedback
+echo -e "\n"
+echo "Positive feedback:"
+echo "=================="
+grep "^pos:" shell-feedback.txt | awk -F: '{print $2}'
+
+# Find and print the negative feedback
+printf "\n\n"
+echo "Negative feedback:"
+echo "=================="
+grep "^neg:" shell-feedback.txt | cut -d':' -f2
+
