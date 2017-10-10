8f41d404a45794024f337aa41b881dd333b45111
Dan Mønster <dan@m0nster.dk>
2017-10-10 15:20:40 +0200

    added output to files

--- sort_feedback.sh
+++ sort_feedback.sh
@@ -2,11 +2,10 @@
 echo -e "\n"
 echo "Positive feedback:"
 echo "=================="
-grep "^pos:" shell-feedback.txt | awk -F: '{print $2}'
+grep "^pos:" shell-feedback.txt | awk -F: '{print $2}' > positive-shell.out
 
 # Find and print the negative feedback
 printf "\n\n"
 echo "Negative feedback:"
 echo "=================="
-grep "^neg:" shell-feedback.txt | cut -d':' -f2
-
+grep "^neg:" shell-feedback.txt | cut -d':' -f2 > negative-shell.out
